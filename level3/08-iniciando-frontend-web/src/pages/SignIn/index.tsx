68ac11432c45127f175cc17550abfb2b733c4d1b
sid <juniorlechado@gmail.com>
2020-07-11 15:07:31 -0300

    Button and Input
    Signed-off-by: sid <juniorlechado@gmail.com>

--- level3/08-iniciando-frontend-web/src/pages/SignIn/index.tsx
+++ level3/08-iniciando-frontend-web/src/pages/SignIn/index.tsx
@@ -3,6 +3,9 @@ import { FiLogIn } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -13,9 +16,9 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" type="text" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
